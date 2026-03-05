a57cf633d1b609d077d8f02075c29cf9d3a99264
haykaysweb <akeredolukolawoleolaoluwa100@gmail.com>
2026-03-05 01:41:10 -0800

    made additional changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,18 +4,18 @@ export default function Home() {
   return (
     <main className="md:min-h-screen flex items-center">
       <div className="container mx-auto px-6 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center">
-          <div className="text-center md:text-left space-y-2">
-            <h1 className="font-semibold text-2xl md:text-4xl lg:text-5xl text-gray-900 leading-tight">
+        <div className="grid grid-cols-1 lg:grid-cols-2 justify-between items-center">
+          <div className="flex flex-col justify center text-center md:text-left space-y-2">
+            <h1 className="text-center lg:text-left font-semibold text-2xl md:text-4xl lg:text-5xl text-gray-900 leading-tight">
               Manage your Tasks on
               <span className="text-purple-600 block mt-2 text-2xl md:text-4xl lg:text-5xl font-semibold">TaskDuty</span>
             </h1>
 
-            <p className="text-sm md:text-xl text-gray-600 max-w-lg mx-auto md:mx-0 leading-relaxed">
+            <p className="text-center lg:text-left w-full text-sm md:text-xl text-gray-600  md:mx-0 leading-relaxed">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non tellus, sapien, morbi ante nunc euismod ac felis ac. Massa et, at platea tempus duis non eget. 
             </p>
 
-            <div className="pt-4">
+            <div className="flex justify-center lg:justify-start items-center lg:items-start w-full pt-4">
               <Link
                 to="/auth/mytask"
                 className="inline-block bg-purple-600 hover:bg-purple-700 transition-colors text-white font-semibold rounded-lg px-4 md:px-8 py-2 md:py-3 shadow-lg shadow-purple-200"
