7159dae059d51e52b7f5ae2c6a02d171d5c3da38
haykaysweb <akeredolukolawoleolaoluwa100@gmail.com>
2026-03-11 13:53:12 -0700

    made major change

---
+++ client/src/Layout/AuthLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router";
+
+export default function AuthLayout() {
+  return (
+    <section className="grid grid-cols-12 items-center justify-center min-h-screen">
+      <div className="hidden md:block col-span-6 h-full">
+        <div
+          className=""
+          style={{
+            backgroundImage: "url('/authBg.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            height: "100vh",
+            overflow: "hidden"
+          }}
+        ></div>
+      </div>
+      <div className="col-span-12 md:col-span-6 p-4 bg-white">
+        <Outlet />
+      </div>
+    </section>
+  );
+}
---
+++ client/src/Routes/AppRoutes.jsx
@@ -0,0 +1,80 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "../Pages/Home/Home";
+import EditTask from "../Pages/Task/EditTask";
+import Newtask from "../Pages/Task/Newtask";
+import Mytask from "../Pages/Task/Mytask";
+import Rootlayout from "../Layout/Rootlayout";
+import AuthLayout from "../Layout/AuthLayout";
+import Register from "../Pages/auth/Register";
+import Login from "../Pages/auth/Login";
+import { Suspense } from "react";
+import Spinner from "../Components/Spinner";
+import { PrivateRoute, PublicRoute } from "./ProtectedRoute";
+import { useAuth } from "../hooks/useAuth";
+
+export default function AppRoutes() {
+  const { accessToken } = useAuth();
+  const route = [
+    {
+      path: "/",
+      element: (
+        <Suspense fallback={<Spinner />}>
+          <Rootlayout />
+        </Suspense>
+      ),
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "auth/newtask",
+          element: (
+            <PrivateRoute accessToken={accessToken}>
+              <Newtask />
+            </PrivateRoute>
+          ),
+        },
+        {
+          path: "auth/editTask",
+          element: (
+            <PrivateRoute accessToken={accessToken}>
+              <EditTask />
+            </PrivateRoute>
+          ),
+        },
+        {
+          path: "auth/mytask",
+          element: (
+            <PrivateRoute accessToken={accessToken}>
+              <Mytask />
+            </PrivateRoute>
+          ),
+        },
+      ],
+    },
+    {
+      element: (
+        <Suspense fallback={<Spinner />}>
+          <PublicRoute accessToken={accessToken}>
+            <AuthLayout />
+          </PublicRoute>
+        </Suspense>
+      ),
+      children: [
+        {
+          path: "auth/register",
+          element: <Register />,
+        },
+        {
+          path: "auth/login",
+          element: <Login />,
+        },
+      ],
+    },
+  ];
+
+  const router = createBrowserRouter(route);
+
+  return <RouterProvider router={router} />;
+}
